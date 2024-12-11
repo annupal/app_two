03ca94fb1f8895077a6e021afc2ab4676f2ecaa2
annupal <anand55666@gmail.com>
2024-12-11 12:39:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import hello from './peek-a-boo.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+     <img src={hello} alt='hello' />     
+      </header>
+      
+    </div>
+  );
+}
+
+export default App;
